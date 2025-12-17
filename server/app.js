a596f8e9a73575f2cbd33d5c072d3d76b6e4e812
rauf-alluviam <intern@novusha.com>
2025-12-17 17:28:34 +0530

    added new clients url

--- server/app.js
+++ server/app.js
@@ -53,6 +53,8 @@ const defaultOrigins = [
   "http://elock-tracking.s3-website.ap-south-1.amazonaws.com",
   "http://localhost:3005",
   "http://eximdev.s3-website.ap-south-1.amazonaws.com",
+  "https://client.alvision.in",
+  "https://d2em7m09618c7h.cloudfront.net",
 ];
 
 const allowedOrigins = [
